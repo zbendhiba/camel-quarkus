d890b62f34d0471316fbc973fdc20c647b705c0c
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-10-12 11:15:54 +0200

    SSL test

--- integration-tests/platform-http-proxy-ssl/src/main/java/org/apache/camel/quarkus/component/platform/http/proxy/ssl/it/Routes.java
+++ integration-tests/platform-http-proxy-ssl/src/main/java/org/apache/camel/quarkus/component/platform/http/proxy/ssl/it/Routes.java
@@ -2,6 +2,7 @@ package org.apache.camel.quarkus.component.platform.http.proxy.ssl.it;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -14,24 +15,9 @@ public class Routes extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-
-
         from("platform-http:proxy")
-                /*  .toD("${headers." + Exchange.HTTP_SCHEME + "}://" +
-                          "${headers." + Exchange.HTTP_HOST + "}:" +
-                          "${headers." + Exchange.HTTP_PORT + "}" +
-                          "${headers." + Exchange.HTTP_PATH + "}?bridgeEndpoint=true");*/
                 .log("header is :: ${headers.CamelHttpHost}")
                 .log("Im here with body :: ${body}")
-                //.transform().constant("hello");
-                .to(url
-                        + "?sslContextParameters=#sslContextParameters&x509HostnameVerifier=#x509HostnameVerifier&bridgeEndpoint=true");
-        
-        
-               /* from("platform-http:/hello")
-                        //.log("url is "+)
-                        .toD(url
-                                + "?bridgeEndpoint=true&sslContextParameters=#ssw");*/
-
+                .toD("https://${headers." + Exchange.HTTP_HOST + "}?bridgeEndpoint=true&sslContextParameters=#sslContextParameters&x509HostnameVerifier=#x509HostnameVerifier");
     }
 }
--- integration-tests/platform-http-proxy-ssl/src/test/java/org.apache.camel.quarkus.component.platform.http.proxy.ssl.it/ProxySslTest.java
+++ integration-tests/platform-http-proxy-ssl/src/test/java/org.apache.camel.quarkus.component.platform.http.proxy.ssl.it/ProxySslTest.java
@@ -12,128 +12,22 @@ import static org.hamcrest.Matchers.equalTo;
 @QuarkusTest
 @QuarkusTestResource(PlatformHttpSSLTestResource.class)
 public class ProxySslTest {
-    /* @Test
-    void testProxy() {
-    
-        final var proxyUrl = "http://localhost:" + RestAssured.port;
-        System.out.println("proxyUrl :: " + proxyUrl);
-    
-        String url = given()
-                .get("/platform-http-proxy")
-                .body().asString();
-    
-        System.out.println("URL is :: " + url);
-        given()
-                .proxy(proxyUrl)
-                .contentType(ContentType.JSON)
-                .when().get(url)
-                .then()
-                .statusCode(200)
-                .body(equalTo("{\"message\": \"Hello World!\"}"));
-    
-        given()
-                .body("hello")
-                .proxy(proxyUrl)
-                .contentType(ContentType.JSON)
-                .when().post(url)
-                .then()
-                .statusCode(200)
-                .body(equalTo("{\"message\": \"Hello World!\"}"));
-    }*/
     @Test
     void test() {
         final var proxyUrl = "http://localhost:" + RestAssured.port;
-        System.out.println("proxyUrl :: " + proxyUrl);
-
-        RestAssured.useRelaxedHTTPSValidation();
-        /*RestAssured.trustStore("ssl/keystore.jks", "changeit");
-
-         RestAssured.config().getSSLConfig().with().keyStore("ssl/keystore.jks", "changeit");*/
-
         String url = given()
                 .get("/platform-http-proxy")
                 .body().asString();
 
-        System.out.println("URL is :: " + url);
+        // forcing RestAssured to send a GET instead of CONNECT with proxy settings
+        url = url.replace("https", "http");
+
         given()
                 .proxy(proxyUrl)
-                .contentType(ContentType.JSON)
-                .when().get(url+"?sslContextParameters=#sslContextParameters&x509HostnameVerifier=#x509HostnameVerifier")
-                .then()
-                .statusCode(200)
-                .body(equalTo("{\"message\": \"Hello World!\"}"));
-    }
-
-    // @Test
-    void test2() {
-        /* final var proxyUrl = "http://localhost:" + RestAssured.port;
-        System.out.println("proxyUrl :: " + proxyUrl);
-        
-        RestAssured.useRelaxedHTTPSValidation();
-        RestAssured.trustStore("ssl/keystore.jks", "changeit");
-        
-        // RestAssured.config().getSSLConfig().with().keyStore("ssl/keystore.jks", "changeit");
-        
-        String url = given()
-                .get("/platform-http-proxy")
-                .body().asString();*/
-
-        //  System.out.println("URL is :: " + url);
-        given()
-                //   .proxy(proxyUrl)
-                .contentType(ContentType.JSON)
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(equalTo("{\"message\": \"Hello World!\"}"));
-    }
-
-    @Test
-    void test3() {
-        final var proxyUrl = "http://localhost:" + RestAssured.port+"/platform-http-proxy/hey";
-        System.out.println("proxyUrl :: " + proxyUrl);
-        given()
-                //   .proxy(proxyUrl)
-                .contentType(ContentType.JSON)
-                .when().get(proxyUrl)
-                .then()
-                .statusCode(200)
-                .body(equalTo("{\"message\": \"Hello World!\"}"));
-    }
-
-    // @Test
-    void testMockURI() {
-
-        RestAssured.useRelaxedHTTPSValidation();
-        //RestAssured.trustStore("ssl/truststore.jks", "changeit");
-        //RestAssured.config().getSSLConfig().with().keyStore("ssl/keystore.jks", "changeit");
-        RestAssured.config().getSSLConfig().with().keyStore("test/baeldung.p12", "changeit");
-
-        String url = given()
-                .get("/platform-http-proxy")
-                .body().asString();
-
-        System.out.println("URL is :: " + url);
-
-        given()
                 .contentType(ContentType.JSON)
                 .when().get(url)
                 .then()
                 .statusCode(200)
                 .body(equalTo("{\"message\": \"Hello World!\"}"));
     }
-
-    /*private static SSLContext buildSSLContextWithTrustStore(KeyStoreSettings trustStoreSettings) {
-        try {
-            KeyStore trustStore = trustStoreSettings.loadStore();
-            return SSLContexts.custom()
-                    .loadTrustMaterial(new TrustSelfSignedStrategy())
-                    .loadKeyMaterial(trustStore, trustStoreSettings.password().toCharArray())
-                    .useTLS()
-                    .build();
-        } catch (Exception e) {
-            return throwUnchecked(e, SSLContext.class);
-        }
-    }*/
-
 }
